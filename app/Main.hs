24a62c1627affb8b7203ce499f38c6db5b409d0e
WildSource <ilovetrap123@hotmail.com>
2025-11-05 12:55:16 -0500

    Define basic BNF

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
+readSourceCode :: [String] -> IO String
+readSourceCode (x:_) = readFile x
+readSourceCode [] = pure ""
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+  sourceCode <- readSourceCode args
+  let codeLines = lines sourceCode
+  print codeLines
+  
