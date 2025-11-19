37f61166556296a5a9dd8a78786efbef3f41d8cd
WildSource <ilovetrap123@hotmail.com>
2025-11-19 12:59:30 -0500

    In progress ast generator

--- app/Parsers.hs
+++ app/Parsers.hs
@@ -1,16 +1,36 @@
 module Parsers where
 
 import Data.Tree
-import Types
-import Parser
+import Types(DataTypes (..))
+import Parser(satisfy, Error (..), Parser (parse))
 import Lexer
 import Control.Applicative ((<|>))
 
 type AST = Tree DataTypes
+type LexerOutput = (Either [Error Char ()] (DataTypes, [Char]))
 
-expressionParser :: Parser Char () [DataTypes]
+isOperator :: DataTypes -> Bool
+isOperator (Operator _) = True
+isOperator _ = False
+
+isNumber :: DataTypes -> Bool
+isNumber (Number _) = True
+isNumber _ = False
+
+isValue :: DataTypes -> Bool
+isValue x = isOperator x || isNumber x
+
+expressionParser :: Parser DataTypes () AST
 expressionParser = do
-  op <- operatorParser
-  num1 <- (numberParser <|> numberParser)
-  num2 <- (numberParser <|> numberParser)
-  pure ([op, num1, num2])
+  op <- satisfy isOperator
+  arg1 <- satisfy isValue
+  arg2 <- satisfy isValue
+  let ast = Node op [Node arg1 [], Node arg2 []]
+  pure ast
+
+generateAST :: String -> AST
+generateAST input =
+  case parse tokenize input of
+    Right (tokens, _) -> parse expressionParser tokens
+    Left err -> error $ show err
+
