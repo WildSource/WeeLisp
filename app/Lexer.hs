ced0f6662c6ee8e047bc23ece7cf752657b8196d
WildSource <ilovetrap123@hotmail.com>
2025-11-18 21:18:24 -0500

    Add Lexer module

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -2,16 +2,13 @@ module Lexer where
 
 import Types
 import Parser
+import Data.Char
 
 digitParser :: Parser Char () DataTypes
-digitParser = Parser $ \ input ->
-  let
-    digitParser' = satisfy isDigit 
-  in
-    case parse digitParser' input of
-      Left err -> Left err
-      Right (x,xs) -> Right (Digit x, xs)
-
+digitParser = Parser $ \ input -> do
+  (x, xs) <- parse (satisfy isDigit) input
+  Right (Digit x, xs)
+  
 operatorParser :: Parser Char () DataTypes
 operatorParser = Parser $ \ input ->
   let operatorParser' = satisfy isOperator
